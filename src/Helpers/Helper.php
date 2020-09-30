0f6edc21e86ab9e4028b18a2d021e25b4ec5168a
ew <rafael.o@aluno.ifsp.edu.br>
2020-09-30 18:39:23 -0300

    Helper que carrega  view

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -3,3 +3,17 @@
 
 namespace TesteHibridoApp\Helpers;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+if (!function_exists('view')) {
+    function view($view = null, $data = [])
+    {
+        $reflection = new \ReflectionClass(\Composer\Autoload\ClassLoader::class);
+        $rootDir = str_replace("/vendor", "", dirname($reflection->getFileName(), 2));
+        $loader = new FilesystemLoader($rootDir . '/resources/views');
+        $twig = new Environment($loader);
+        return $twig->render($view, $data);
+
+    }
+}
